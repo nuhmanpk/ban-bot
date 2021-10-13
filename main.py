911e8fcaf2ea24b982d0362b636628815ff39f58
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-10-13 08:07:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+
 import pyrogram
-import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
@@ -10,21 +10,37 @@ bughunter0 = Client(
     api_hash=os.environ["API_HASH"],
 )
 
- if message.reply_to_message:
-  admins = await bot.get_chat_members(chat_id=chatid, filter="administrators")
-  userid = message.from_user.id
-  if userid in admins:    
+
+@bughunter0.on_message(filters.command(["ban"]))
+async def ban(bot, message):
     chatid = message.chat.id
-    user_to_ban = message.reply_to_message.from_user.id
-    if user_to_ban in admins:
-        await message.reply(text="Think he is Admin, Can't Ban Admins")   
+    if message.reply_to_message:
+        admins_list = await bot.get_chat_members(
+            chat_id=chatid, filter="administrators"
+        )
+        admins = []
+        for admin in admins_list:
+            id = admin.user.id
+            admins.append(id)
+        userid = message.from_user.id
+        if userid in admins:
+            user_to_ban = message.reply_to_message.from_user.id
+            if user_to_ban in admins:
+                await message.reply(text="Think he is Admin, Can't Ban Admins")
+            else:
+                try:
+                    await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban)
+                    await message.reply_text(
+                        f"Bye {message.reply_to_message.from_user.mention}"
+                    )
+                except Exception as error:
+                    await message.reply_text(f"{error}")
+        else:
+            await message.reply_text("Nice try, But wrong move..")
+            return
     else:
-        try:  
-            await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban)
-        except Exception as error:
-            await message.reply_text(f"{error")
-  else:
-      await message.reply_text("Nice try, But wrong move..")
-      return
- else:
-    return
+        return
+
+
+bughunter0.run()
+
