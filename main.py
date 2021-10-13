f2228fef0994c0027a991a1d0d86ac27f53d78a7
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-13 10:44:10 +0530

    Update ban

--- main.py
+++ main.py
@@ -14,33 +14,43 @@ bughunter0 = Client(
 
 @bughunter0.on_message(filters.command(["ban"]))
 async def ban(bot, message):
-    chatid = message.chat.id
-    if message.reply_to_message:
-        admins_list = await bot.get_chat_members(
-            chat_id=chatid, filter="administrators"
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user = await bot.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_id
+    )
+    if user.can_restrict_members is False:
+        await message.reply_text(
+            text="You can't do that!"
         )
-        admins = []
-        for admin in admins_list:
-            id = admin.user.id
-            admins.append(id)
-        userid = message.from_user.id
-        if userid in admins:
-            user_to_ban = message.reply_to_message.from_user.id
-            if user_to_ban in admins:
-                await message.reply(text="Think he is Admin, Can't Ban Admins")
-            else:
-                try:
-                    await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban)
-                    await message.reply_text(
-                        f"Bye {message.reply_to_message.from_user.mention}"
-                    )
-                except Exception as error:
-                    await message.reply_text(f"{error}")
-        else:
-            await message.reply_text("Nice try, But wrong move..")
-            return
-    else:
         return
+    if message.reply_to_message:
+        user_to_ban_id = message.reply_to_message.from_user.id
+    else:
+        if len(message.text.split()) > 1:
+            user_to_ban_id = message.text.split()[1]  # you can ban with "/ban id"
+        else:
+            await message.reply_text(
+                text="Send command with id of user to ban."
+            )
+            return
+    user_to_ban = await bot.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_to_ban_id
+    )
+    if user_to_ban.status == "administrator":
+        await message.reply(
+            text="He is an admin of this chat, So I can't ban admins."
+        )
+    else:
+        try:
+            await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban_id)
+            await message.reply_text(
+                text=f"Banned {message.reply_to_message.from_user.mention}!"
+            )
+        except Exception as error:
+            await message.reply_text(error)
 
 
 bughunter0.run()
